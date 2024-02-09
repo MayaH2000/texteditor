a56fc892f7956bec751dc0d12ce1695c4977a946
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-02-08 19:33:26 -0600

    added click event handler on butInstall, beforeinstallprompt, and appinstalled events

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,32 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  event.preventDefault();
+  // Stash the event so it can be triggered later
+  deferredPrompt = event;
+  // Update UI to notify the user they can add to home screen
+  showInstallPromotion();
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const choiceResult = await deferredPrompt.userChoice;
+  if (choiceResult.outcome === 'accepted') {
+    console.log('User accepted the A2HS prompt');
+  } else {
+    console.log('User dismissed the A2HS prompt');
+  }
+  // Clear the deferredPrompt variable as it's a one-time event
+  deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  // Log when the app is installed.
+  console.log('App installed successfully.');
+});
